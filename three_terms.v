fbdfd9d7a107ba75d6dccfc56376b0abb7f22146
Riku Sakamoto <rikusaka03@gmail.com>
2019-02-06 22:57:58 +0900

    コメントを追加

--- three_terms.v
+++ three_terms.v
@@ -19,6 +19,7 @@ Definition DoChangeStatus(b1 b2:bool)(n:nat):nat :=
  | (false, false) => S (S (S (S n)))
  end.
 
+(* 3つの入力に対してReDo関数を実行 *)
 Fixpoint DoUpdateFunction (b1 b2 b3: bool)(n:nat):nat :=
  DoChangeStatus b1 b2 (DoChangeStatus b2 b3 n). 
 
@@ -33,37 +34,49 @@ Definition UnDoChangeStatus(b1 b2:bool)(x:nat):nat :=
  | (false, false) => x - 1
  end.
 
-
+(* 3つの入力に対してUnDo関数を実行 *)
 Fixpoint UnDoUpdateFunction (b1 b2 b3 :bool)(n:nat):nat :=
  UnDoChangeStatus b2 b1 (UnDoChangeStatus b3 b2 n). 
 
-
+(* 試しに計算 *)
 Compute DoUpdateFunction true true false 3.
 Compute UnDoUpdateFunction true false false 6.
 
+(*Boolを反転させる*)
 Definition rev_bool (a:bool):bool :=
   match a with
   |true => false
   |false => true
  end.
 
+(* 小定理 1 *)
+(* ReDo(n+1) = ReDo(n) + 1 *)
 Theorem DoSn : forall (a b:bool) (n:nat), 
   DoChangeStatus a b (S n) =  S (DoChangeStatus a b n).
 
+(* 方針：a,b2つのBool値について地道に場合分け *)
+
 Proof.
 intros.
-case a.
-case b.
+case a. (* aについて地道に場合分け *)
+
+(* a=true の時*)
+case b. (* bについて地道に場合分け *)
 -
+(* b=true の時*)
 intros.
 simpl.
-induction n.
+induction n. (* nに関する帰納法*)
+(* n=nil の時*)
 simpl.
 reflexivity.
 
+(* 一般nの時*)
 simpl.
 reflexivity.
 -
+(* a=falseの時 *)
+(* a=後は同様 *)
 intros.
 simpl.
 induction n.
@@ -90,6 +103,8 @@ reflexivity.
 reflexivity.
 Qed.
 
+(* 小定理 2 *)
+(* Undo(ReDo(n)) = n *)
 Theorem ReDoUnDo : forall (a b:bool) (n:nat), 
   UnDoChangeStatus (rev_bool a) (rev_bool b) (DoChangeStatus a b n) = n.
 
@@ -99,11 +114,14 @@ case a.
 case b.
 simpl.
 
+(* nに関する帰納法*)
 induction n.
 -
 simpl.
 auto.
 -
+(* n=n+1の時 *)
+(*小定理１を使って式を分解*)
 rewrite DoSn.
 simpl.
 auto.
@@ -135,6 +153,8 @@ rewrite DoSn.
 auto.
 Qed.
 
+(* 小定理 3 *)
+(* Undo(a b c n) = UnDoChangeStatus a b UnDoChangeStatus(b c n) *)
 Theorem DecompositionUnDo : forall (a b c:bool) (n:nat),
   UnDoUpdateFunction a b c n =  UnDoChangeStatus b a (UnDoChangeStatus c b n).
 
@@ -147,6 +167,8 @@ simpl.
 reflexivity.
 Qed.
 
+(* 小定理 4 *)
+(* Do(a b c n) = DoChangeStatus a b DoChangeStatus(b c n) *)
 Theorem DecompositionDo : forall (a b c:bool) (n:nat),
   DoUpdateFunction a b c n =  DoChangeStatus a b (DoChangeStatus b c n).
 
@@ -165,8 +187,11 @@ Theorem ReDoUnDoFunction : forall (a b c:bool) (n:nat),
 
 Proof.
 intros.
+(* 小定理3より、UnDoUpdateFunctionを展開 *)
 rewrite DecompositionUnDo.
+(* 小定理4より、DoUpdateFunctionを展開 *)
 rewrite DecompositionDo.
+(* 小定理2より、DoしてUnDoしたら元に戻る *)
 rewrite ReDoUnDo.
 rewrite ReDoUnDo.
 reflexivity.
